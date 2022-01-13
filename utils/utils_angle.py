6cd94c8cf5b1ea1ca26d41c4a230d70d08cc570d
Rii(IIM) <02ryoji19@s.okayama-u.ac.jp>
2022-01-13 10:27:04 +0900

    improvement(graphs, coordinate output, cog estimation)

--- utils/utils_angle.py
+++ utils/utils_angle.py
@@ -3,6 +3,12 @@ import pandas as pd
 import csv
 import matplotlib.pyplot as plt
 
+import os
+import os.path as osp
+import sys
+sys.path.insert(0, osp.abspath('../'))
+from utils.utils_calculation import rounding, round_dp2
+
 est_list = []
 csv_stack = []
 
@@ -82,38 +88,82 @@ def output(list):
     print("theta: %s\n"%(list[13]))
     print("degree: %s\n"%(list[14]))
 
-def angleplt(input_name, x, y):
+def angleplt(input_name, x, y, fps):
+    x_sec = []
     save_path = './graph/' + input_name + '_graph.png'
+    plt_title = "Angle Transiton (" + input_name + ')'
+    
+    for a in range(len(x)):
+        sec = a / fps
+        x_sec.append(round(float(sec), 2))
+
     plt.figure()
-    plt.title("Angle per frame")
-    plt.xlabel("frame_num")
-    plt.ylabel("angle")
-    plt.xlim(0, len(x))
+    plt.title(plt_title)
+    plt.xlabel("second(s)")
+    plt.ylabel("angle(deg)")
+    plt.xlim(0, np.max(x_sec))
     plt.ylim(0, 180)
     plt.grid(True)
-    plt.plot(x, y)
+    plt.plot(x_sec, y)
     plt.savefig(save_path)
+    plt.close()
 
-def angleplt_smo(input_name, x, y):
+def angleplt_smo(input_name, x, y, fps):
+    for i in range(len(x)):
+        n = y[i]
+        if np.isnan(n):
+            print(n)
+            b, a = y[i-5:i-1], y[i+1:i+5]
+            b_ave, a_ave = np.nanmean(np.nan_to_num(b)), np.nanmean(np.nan_to_num(a))
+            if b_ave == 0:
+                tmp = float(a_ave)
+            elif a_ave == 0:
+                tmp = float(b_ave)
+            else:
+                tmp = float((b_ave + a_ave) / 2)
+            print(b)
+            print(a)
+            print(b_ave)
+            print(a_ave)
+            print(tmp)
+            y[i] = round(tmp, 2)
+        i = i + 1
+
+    angleplt(input_name, x, smoothing(y), fps)
+
+
+def angleplt_two(input_name, x, y1, y2, fps):
+    x_sec = []
+    c1,c2 = "cyan","orange"     # 各プロットの色
+    l1,l2 = "x_cog","y_cog" 
     save_path = './graph/' + input_name + '_graph.png'
-    plt.figure()
-    plt.title("Angle per frame")
-    plt.xlabel("frame_num")
-    plt.ylabel("angle")
-    plt.xlim(0, len(x))
-    plt.ylim(0, 180)
-    plt.grid(True)
-    plt.plot(x, smoothing(y))
-    plt.savefig(save_path)
+    plt_title = "Center of Gravity(COG) (" + input_name + ")"
 
-def coordplt(x, y_1, y_2, plt_path):
-    frame_per_sec = 25
-    frame_num = len(x)
+    
+    for a in range(len(x)):
+        sec = a / fps
+        x_sec.append(round(float(sec), 2))
+
+    fig, ax = plt.subplots()
+    plt.figure()
+    ax.set_title(plt_title)
+    ax.set_xlabel("second(s)")
+    ax.set_ylabel("angle(deg)")
+    plt.xlim(0, np.max(x_sec))
+    plt.ylim(0, 180)
+    ax.grid(True)
+    ax.plot(x_sec, y1, color=c1, label=l1)
+    ax.plot(x_sec, y2, color=c2, label=l2)
+    plt.savefig(save_path)
+    plt.close()
+
+
+def coordplt(x, y_1, y_2, plt_path, fps):
     sec_list = []
 
-    for n in range(frame_num):
-        fps = x[n] / frame_per_sec
-        sec_list.append(round((float(fps)), 2))
+    for n in range(len(x)):
+        sec = n / fps
+        sec_list.append(round((float(sec)), 2))
 
     plt.title("trandition")
     plt.xlabel("time[s]")
@@ -129,28 +179,28 @@ def coordplt(x, y_1, y_2, plt_path):
     plt.close()
 
 
-def trandition(input_name, angle_info, output_list):
+def stack_coords(stack_list, angle_info):
+    tmp_list = []
+    for i in range(17):
+        for j in range(2):
+            x = angle_info[i][j]
+            tmp_list.append(round(x, 2))
+            j = j + 1
+        i = i + 1
+    stack_list.append(tmp_list)
+
+def trandition(input_name, output_list):
     header_list = ['nose_x', 'nose_y', \
                    'left_eye_x', 'left_eye_y', 'right_eye_x', 'right_eye_y',\
                    'left_ear_x', 'left_ear_y', 'right_ear_x', 'right_ear_y',\
                    'left_shoulder_x', 'left_shoulder_y', 'right_shoulder_x', 'right_shoulder_y',\
                    'left_elbow_x', 'left_elbow_y', 'right_elbow_x', 'right_elbow_y',\
                    'left_wrist_x', 'left_wrist_y', 'right_wrist_x', 'right_wrist_y',\
-                   'left_hip_x', 'left_hip_y', 'right_hip_x', 'right_hip_y'\
+                   'left_hip_x', 'left_hip_y', 'right_hip_x', 'right_hip_y',\
                    'left_knee_x', 'left_knee_y', 'right_knee_x', 'right_knee_y',\
                    'left_ankle_x', 'left_ankle_y', 'right_ankle_x', 'right_ankle_y']
     x: float = 0.0
 
     save_path = input_name + '_plot.csv'
-
-    with open(save_path, 'w', newline='') as f:
-        writer = csv.writer(f, lineterminator='\n')
-        tmp_list = []
-        for i in range(17):
-            for j in range(2):
-                x = angle_info[i][j]
-                tmp_list.append(round(x, 2))
-                j = j + 1
-            i = i + 1
-        writer.writerow(tmp_list)
-        output_list.append(tmp_list)
+    df = pd.DataFrame(output_list, columns=header_list)
+    df.to_csv(save_path)
--- utils/utils_peek.py
+++ utils/utils_peek.py
@@ -1,32 +1,42 @@
 import numpy as np
 import pandas as pd
 
+import os
+import os.path as osp
+import sys
+sys.path.insert(0, osp.abspath('../'))
+from utils.utils_calculation import rounding, round_dp2
 
-def angle_peek(file_name, angle_list):
+
+def angle_peek(file_name, angle_list, fps):
+    save_path = "./peek/" + file_name + "_peek.csv"
 
     ang_max = np.nanmax(angle_list)
     ang_min = np.nanmin(angle_list)
-    ind_max = np.nanargmax(angle_list)
-    ind_min = np.nanargmin(angle_list)
+    idx_max = np.nanargmax(angle_list)
+    idx_min = np.nanargmin(angle_list)
+    sec_max = round(float(idx_max / fps), 2)
+    sec_min = round(float(idx_min / fps), 2)
+    ang_dif = round(float(abs(ang_max - ang_min)), 2)
+    idx_dif = abs(idx_max - idx_min)
+    sec_dif = round(float(abs(sec_max - sec_min)), 2)
 
-    ang_dif = abs(ang_max - ang_min)
-    ind_dif = abs(ind_max - ind_min)
+    header=['ang_max', 'ang_min', 'ind_max', 'ind_min', 'sec_max', 'sec_min', 'ang_dif', 'ind_dif', 'sec_dif']
+    peek_data = [ang_max, ang_min, idx_max, idx_min, sec_max, sec_min, ang_dif, idx_dif, sec_dif]
+    
 
-    cul_data = {'header':['ang_max', 'ang_min', 'ind_max', 'ind_min', 'ang_dif', 'ind_dif'],
-                'data': [ang_max, ang_min, ind_max, ind_min, ang_dif, ind_dif]
-               }
-
-    save_path = "./peek/" + file_name + "_peek.csv"
-
-    df = pd.DataFrame(cul_data)
-    df.to_csv(save_path)
+    df = pd.DataFrame([peek_data], columns=header)
+    df.to_csv(save_path,index=False)
 
 
 
-    print("---* " + str(file_name) + " *---")
-    print("maximum angle: " + str(ang_max) + " (degree)")
-    print("minimum angle: " + str(ang_min) + " (degree)")
-    print("maximum index: " + str(ind_max) + " (frame number)")
-    print("minimum index: " + str(ind_min) + " (frame number)")
-    print("difference(angle): " + str(ang_dif) + " (degree)")
-    print("difference(index): " + str(ind_dif) + " (frame namber)")
+    # print("---* " + str(file_name) + " *---")
+    print("maximum angle : " + str(ang_max) + " (degree)")
+    print("minimum angle : " + str(ang_min) + " (degree)")
+    print("maximum index : " + str(idx_max) + " (frame number)")
+    print("minimum index : " + str(idx_min) + " (frame number)")
+    print("maximum second: " + str(sec_max) + " (frame number)")
+    print("minimum second: " + str(sec_min) + " (frame number)")
+    print("difference(angle) : " + str(ang_dif) + " (degree)")
+    print("difference(index) : " + str(idx_dif) + " (frame namber)")
+    print("difference(second): " + str(idx_dif) + " (second)")
